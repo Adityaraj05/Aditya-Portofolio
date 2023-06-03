2eecacde1cdd24dc861bc0932abdff2ff85ae026
Aditya raj <adityarajavi05@gmail.com>
2023-06-03 09:45:20 +0530

    Completed

--- src/App.js
+++ src/App.js
@@ -1,42 +1,54 @@
-import { ThemeProvider } from "styled-components"
-import { lightTheme } from "./components/Themes"
-import {  Route,
-    Routes, } from "react-router-dom";
-import GlobalStyle from "./globalStyles"
-//components
-import Main from './components/Main'
-import BlogPage from "./components/BlogPage";
-import MySkillsPage from "./components/MySkillsPage";
-import AboutPage from "./components/AboutPage";
-import WorkPage from "./components/WorkPage";
+import { Routes, Route, useLocation } from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
+import { lazy, Suspense } from "react";
+import GlobalStyle from "./globalStyles";
+import { ThemeProvider } from "styled-components";
+import { lightTheme } from "./components/Themes";
+import Loading from "./subComponents/Loading";
+import React from "react";
 
+//Components
+const Main = lazy(() => import("./components/Main"));
+const AboutPage = lazy(() => import("./components/AboutPage"));
+const MySkillsPage = lazy(() => import("./components/MySkillsPage"));
+const BlogPage = lazy(() => import("./components/BlogPage"));
+const WorkPage = lazy(() => import("./components/WorkPage"));
+const SoundBar = lazy(() => import("./subComponents/SoundBar"));
 
 function App() {
-  return <>
-  
-  
+  const location = useLocation();
 
-    <GlobalStyle />
+  return (
+    <>
+      <GlobalStyle />
 
+      <ThemeProvider theme={lightTheme}>
+        <Suspense fallback={<Loading />}>
+          <SoundBar />
+          {/* Changed prop from exitBefore to mode */}
+          <AnimatePresence mode="wait">
+            {/* Changed Switch to Routes */}
 
-    <ThemeProvider theme={lightTheme}>
-    
-    <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/about" element={ <AboutPage />} />
-        <Route path="/blog" element={<BlogPage />} />
-        <Route path="/work" element={<WorkPage />} />
-        <Route path="/skills" element={<MySkillsPage />} />
-    </Routes>
-   
-    
-    </ThemeProvider>
+            <Routes location={location} key={location.pathname}>
+              {/* Changed component to element */}
 
+              <Route path="/" element={<Main />} />
 
+              <Route path="/about" element={<AboutPage />} />
 
+              <Route path="/blog" element={<BlogPage />} />
 
-  </>
+              <Route path="/work" element={<WorkPage />} />
 
+              <Route path="/skills" element={<MySkillsPage />} />
+
+              <Route path="*" element={<Main />} />
+            </Routes>
+          </AnimatePresence>
+        </Suspense>
+      </ThemeProvider>
+    </>
+  );
 }
 
-export default App
+export default App;
